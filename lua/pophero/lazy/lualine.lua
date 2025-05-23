a6e3cf8acacccc48e7878d07993706e5602220ad
pophero110 <ojn2014520@gmail.com>
2025-05-22 21:00:59 -0500

    add total line of code to status line

--- lua/pophero/lazy/lualine.lua
+++ lua/pophero/lazy/lualine.lua
@@ -2,8 +2,17 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local function buffer_line_count()
+      return " " .. vim.api.nvim_buf_line_count(0)
+    end
+
     require("lualine").setup({
-      theme = "tokyonight"
+      theme = "tokyonight",
+      sections = {
+        lualine_c = {
+          buffer_line_count
+        }
+      }
     })
   end
 }
