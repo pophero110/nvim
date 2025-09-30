b27abb497618e7d4f47a74fda8cb6bb2e88401d9
Jiajin.Ou <jiajin.ou@cvshealth.com>
2025-09-30 08:26:35 -0500

    save default iterms profile

--- lua/pophero/lazy/auto-save.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Pocco81/auto-save.nvim",
-  config = function()
-    require("auto-save").setup({
-      trigger_events = { "InsertLeave" },
-      debounce_delay = 2000
-    })
-  end
-}
--- lua/pophero/lazy/conform.lua
+++ lua/pophero/lazy/conform.lua
@@ -1,27 +1,27 @@
 return {
-  'stevearc/conform.nvim',
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  opts = {
-    formatters = {
-      biome_format = {
-        command = "biome",
-        args = { "format", "--write", "--stdin-file-path", "$FILENAME" },
-      },
-    },
-    formatters_by_ft = {
-      javascript = { "biome_format" },
-      javascriptreact = { "biome_format" },
-      typescript = { "biome_format" },
-      typescriptreact = { "biome_format" },
-    },
-    -- Set default options
-    default_format_ops = {
-      lsp_format = "fallback",
-    },
-    -- Set up format-on-save
-    format_on_save = {
-      timeout_ms = 500
-    }
-  }
+	'stevearc/conform.nvim',
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters = {
+			-- biome_format = {
+			-- 	command = "biome",
+			-- 	args = { "format", "--write", "--stdin-file-path", "$FILENAME" },
+			-- },
+		},
+		formatters_by_ft = {
+			-- javascript = { "biome_format" },
+			-- javascriptreact = { "biome_format" },
+			-- typescript = { "biome_format" },
+			-- typescriptreact = { "biome_format" },
+		},
+		-- Set default options
+		default_format_ops = {
+			lsp_format = "fallback",
+		},
+		-- Set up format-on-save
+		format_on_save = {
+			timeout_ms = 500
+		}
+	}
 }
